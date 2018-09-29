4639cf1aa308549687a5b6f45d0ce50267a1db5b
Erik <erik.steinman@hotmail.com>
2018-09-29 12:23:46 +0200

    application/json response setup"

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import jsonify
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/events")
+def events():
+    return app.response_class(response=json.dumps({"name": "hej"}), mimetype="application/json")
